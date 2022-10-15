abd837704607c72afa67e79713fe92fb94caf2f0
intelligentdnb <juandiarts@gmail.com>
2022-10-15 20:49:30 -0300

    adding emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,11 +8,11 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        emailjs.sendForm('service_7qfl2wn', 'template_3bdufol', e.target, 'C3e82jUVt-Q_Ju8sJ')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
-                console.log(error.text);
+                console.log(error.text);    
             });
     };
 
