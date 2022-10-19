78f501eca98122c9d477a9c6f6ca391711ef8955
intelligentdnb <juandiarts@gmail.com>
2022-10-19 01:34:36 -0300

    css/contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import Footer from '../footer/Footer';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {AiFillCopy} from "react-icons/ai"
+import "./contact.css"
 
 const Contact = () => {
     const form = useRef();
@@ -12,22 +15,39 @@ const Contact = () => {
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
-                console.log(error.text);    
+                console.log(error.text);
             });
     };
 
     return (
         <div>
-            <form ref={form} onSubmit={sendEmail}>
-                <label>Name</label>
-                <input type="text" name="user_name" />
-                <label>Email</label>
-                <input type="email" name="user_email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input type="submit" value="Send" />
-            </form>
-            <Footer />
+            <div className='invisible_header'></div>
+            <div className='"contact_container"'>
+                <div>
+                    <form ref={form} onSubmit={sendEmail} className="container_form">
+                        <label>your name</label>
+                        <input type="text" name="user_name" className='user_name' />
+                        <label>your email</label>
+                        <input type="email" name="user_email" className='user_email' />
+                        <label>your message to me</label>
+                        <textarea name="message" className='message' />
+                        <input type="submit" value="send to my email" className='button' />
+                    </form>
+                </div>
+                <div className='contact_list_container'>
+                    <ul>
+                        <p>socials</p>
+                        <li>github</li>
+                        <li>linkedin</li>
+                        <br/>
+                        <p>email</p>
+                        <li>juandiarts@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div className='contact_footer'>
+                <Footer />
+            </div>
         </div>
     );
 }
